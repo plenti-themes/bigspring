17232daef922a77fe18fd60eb05779f72885aabd
jimafisk <jimafisk@gmail.com>
2021-04-05 19:55:40 -0400

    Start blurb comp.

---
+++ layouts/components/blurb.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let title, paragraphs;
+</script>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-10 mx-auto">
+          <div class="content">
+<h4>{title}</h4>
+
+{#each paragraphs as p}
+    <p>{@html p}</p>
+{/each}
+
+</div>
+</div>
+</div>
+</div>
