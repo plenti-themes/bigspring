6e690ce43bcdf0ab2cffcb6ca6218f01abfac4d8
jimafisk <jimafisk@gmail.com>
2021-04-05 20:11:34 -0400

    Fix blurbs comp.

---
+++ layouts/components/blurbs.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let items;
+</script>
+
+<section class="section">
+  <div class="container">
+    {#each items as item}
+    <div class="row">
+      <div class="col-lg-10 mx-auto">
+        <div class="content">
+          <h4>{item.title}</h4>
+          {#each item.paragraphs as p}
+            <p>{@html p}</p>
+          {/each}
+        </div>
+      </div>
+    </div>
+    {/each}
+  </div>
+</section>
