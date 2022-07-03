e5d92333a3b0d204155cd8f3737705a7e24e02b6
jimafisk <jimafisk@gmail.com>
2022-07-02 22:43:19 -0400

    Add adminMenu.

--- layouts/global/html.svelte
+++ layouts/global/html.svelte
@@ -3,18 +3,23 @@
   import Nav from './nav.svelte';
   import Footer from './footer.svelte';
 
-  export let layout, content, allContent, allLayouts, env;
+  export let layout, content, allContent, allLayouts, env, user, adminMenu;
 </script>
 
 <html lang="en">
   <Head title={content.filename} {env} {...content.fields} />
 <body>
-  <header class="navigation">
-    <div class="container">
-      <Nav />
-    </div>
-  </header>
-  <svelte:component this={layout} {...content.fields} {allContent} {allLayouts} {content} />
-  <Footer />
+  {#if user && $user.isAuthenticated}
+    <svelte:component this={adminMenu} {user} bind:content={content} />
+  {/if}
+  <main>
+    <header class="navigation">
+      <div class="container">
+        <Nav />
+      </div>
+    </header>
+    <svelte:component this={layout} {...content.fields} {allContent} {allLayouts} {content} />
+    <Footer />
+  </main>
 </body>
 </html>
